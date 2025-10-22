ac905cc9d8d527fc68bf2e9186bc1e5d961bb553
Lan Ly <lanly-dev@users.noreply.github.com>
2025-10-22 10:57:39 -0700

    add: view/item/context

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,12 @@
 import * as vscode from 'vscode'
+import { StatusTreeItem, StatusTreeDataProvider } from './treeview'
 
 export function activate(context: vscode.ExtensionContext) {
 
-	// Output diagnostic information
-	console.log('Congratulations, your extension "ai" is now active!')
-
-	// Register the command to open the Hunyuan 3D login page in a Webview
-	const openLoginWebview = vscode.commands.registerCommand('ai.openHunyuanLogin', () => {
+	const d1 = vscode.commands.registerCommand('ai.hunyuanLogin', () => {
 		const panel = vscode.window.createWebviewPanel(
 			'hunyuanLogin',
-			'Hunyuan 3D Login',
+			'Hunyuan Login',
 			vscode.ViewColumn.One,
 			{
 				enableScripts: true
@@ -30,52 +27,14 @@ export function activate(context: vscode.ExtensionContext) {
 			</html>
 		`
 	})
-	context.subscriptions.push(openLoginWebview)
-
-	// Tree Item for status
-	class StatusTreeItem extends vscode.TreeItem {
-		constructor(label: string) {
-			super(label, vscode.TreeItemCollapsibleState.None)
-			this.contextValue = 'statusItem'
-			this.command = {
-				command: 'ai.openHunyuanLogin',
-				title: 'Login',
-				tooltip: 'Open Hunyuan 3D Login'
-			}
-			this.description = 'Not Login'
-			this.iconPath = new vscode.ThemeIcon('account')
-			// Inline buttons are not supported in TreeItem; use command for click action
-		}
-	}
-
-	// Tree Data Provider
-	class StatusTreeDataProvider implements vscode.TreeDataProvider<StatusTreeItem> {
-		getTreeItem(element: StatusTreeItem): vscode.TreeItem {
-			return element
-		}
-		getChildren(element?: StatusTreeItem): Thenable<StatusTreeItem[]> {
-			if (!element) {
-				return Promise.resolve([
-					new StatusTreeItem('Not Login')
-				])
-			}
-			return Promise.resolve([])
-		}
-	}
 
 	// Register the TreeView
-	const statusTreeView = vscode.window.createTreeView('aiStatusTree', {
+	const statusTreeView = vscode.window.createTreeView('aiTreeView', {
 		treeDataProvider: new StatusTreeDataProvider(),
 		showCollapseAll: false
 	})
-	context.subscriptions.push(statusTreeView)
-
-	// Keep the original helloWorld command
-	const disposable = vscode.commands.registerCommand('ai.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from ai!')
-	})
-	context.subscriptions.push(disposable)
+	context.subscriptions.push(statusTreeView, d1)
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
---
+++ src/treeview.ts
@@ -0,0 +1,32 @@
+import * as vscode from 'vscode'
+
+export class StatusTreeItem extends vscode.TreeItem {
+  constructor(label: string) {
+    super(label, vscode.TreeItemCollapsibleState.None)
+    this.contextValue = 'statusItem'
+    this.command = {
+      command: 'ai.hunyuanLogin',
+      title: 'Login',
+      tooltip: 'Open Hunyuan 3D Login',
+    }
+    this.description = 'Not Login'
+    this.iconPath = new vscode.ThemeIcon('account')
+    this.contextValue = 'ai.notLoggedIn'
+  }
+}
+
+export class StatusTreeDataProvider implements vscode.TreeDataProvider<StatusTreeItem> {
+
+  getTreeItem(element: StatusTreeItem): vscode.TreeItem {
+    return element
+  }
+
+  getChildren(element?: StatusTreeItem): Promise<StatusTreeItem[]> {
+    if (!element) {
+      return Promise.resolve([
+        new StatusTreeItem('Not Login')
+      ])
+    }
+    return Promise.resolve([])
+  }
+}
